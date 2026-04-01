9af350d4aca1e8728ba3600e64243de963e6bc02
rajshekhar45 <raajsh00@gmail.com>
2026-04-02 05:06:46 +0530

    fixed indentation error

--- web.py
+++ web.py
@@ -1,32 +1,29 @@
- from flask import Flask, request
+from flask import Flask,request
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        
-        # Check if file part exists
+
         if "image" not in request.files:
-            return "No file part ❌"
+            return "No file part❌"
 
         file = request.files["image"]
 
-        # Check if file is selected
         if file.filename == "":
-            return "No file selected ❌"
+            return "No file selected❌"
 
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(UPLOAD_FOLDER, filename)
-            file.save(filepath)
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(filepath)
 
-            return f"Uploaded: {filename} ✅"
+        return f"Uploaded: {filename}🚗"
 
     return '''
     <h2>Upload Image 🚗</h2>
